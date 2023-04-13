90cd13ad377d737c7433cdcf9662bb5830c428b1
gi <gik044107@gmail.com>
2023-04-13 16:46:23 +0900

    xml 예제 커밋

--- java-playground/src/main/java/example/xml/XmlExample.java
+++ java-playground/src/main/java/example/xml/XmlExample.java
@@ -1,10 +1,145 @@
 package example.xml;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class XmlExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		XmlExample xmlExample = new XmlExample();
+		String filePath = "C:/zipTest/test3/Contents/section0.xml";
+		String text ="문서 제목 [HY헤드라인M 22pt]";
+		String id = "documentTitle";
+	      try {
+	    	  Document document = xmlExample.parseXmlFile(filePath);
+	          
+	          System.out.println(xmlExample.getNodeTextContentById(document, id));
+	          if(xmlExample.setNodeTextContentById(document, id,  text)) {
+	        	  System.out.println(xmlExample.getNodeTextContentById(document, id));
+	          }
+	          
+	          xmlExample.writeXmlFile(document, filePath);
+	          
+	      } catch (Exception e) {
+	          e.printStackTrace();
+	      } 
 
 	}
+	
+	public void writeXmlFile(Document document, String filePath) throws Exception {
+        DOMSource source = new DOMSource(document);
+        FileWriter writer = null;
+		try {
+			writer = new FileWriter(new File(filePath));
+			StreamResult result = new StreamResult(writer);
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.transform(source, result);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		}catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+			throw e;
+		} catch (TransformerException e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+	public boolean setNodeTextContentById(Document document, String id, String text) {
+		Node node =getNodeById(document, id);
+		if(node==null) {
+			return false;
+		}else {
+			node.setTextContent(text);
+			return true;
+		}
+	}
+	
+	public String getNodeTextContentById(Document document, String id) {
+		Node node =getNodeById(document, id);
+		if(node==null) {
+			return "";
+		}else {
+			return node.getTextContent();
+		}
+	}
+	
+	public Node getNodeById(Document document, String id) {
+		if(document==null||id==null||id.isEmpty()) {
+			return null;
+		}
+		Element documentElement = document.getDocumentElement();
+		return getNodeById(documentElement.getChildNodes(), id);
+	}
 
+	public Node getNodeById(NodeList childNodes, String id) {
+		if(childNodes!=null&&childNodes.getLength()>0) {
+			for(int i=0, length =childNodes.getLength(); i<length; i++) {
+				Node child = childNodes.item(i);
+				if(isTheNode(child ,id)) {
+					return child;
+				}else {
+					Node childNode = getNodeById(child.getChildNodes(), id);
+					if(childNode!=null) {
+						return childNode;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	public boolean isTheNode(Node child , String id) {
+		if(child!=null&&id!=null&&!id.isEmpty()) {
+			NamedNodeMap nodeAttributeKeys = child.getAttributes();
+			if(nodeAttributeKeys!=null) {
+				Node idNode =nodeAttributeKeys.getNamedItem("id");
+				if(idNode!=null) {
+					return idNode.getNodeValue().equals(id);
+				}
+			}
+		}
+		return false;
+	}
+	
+	public Document parseXmlFile(String filePath) throws Exception {
+		File file = new File(filePath);
+		if(!file.exists()) {
+			return null;
+		}
+	    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+	    Document document = null;
+	    try {
+	    	documentBuilderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+	        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+	        document = documentBuilder.parse(file);
+	        document.getDocumentElement().normalize();
+	    } catch (ParserConfigurationException | SAXException | IOException e) {
+	    	e.printStackTrace();
+	    	throw e;
+	    } 
+	    return document;
+	}
 }
